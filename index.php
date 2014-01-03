82ec9a463fd5f85cf1dd58c7e6b3de5103164def
sa <s.angeard@laposte.net>
2014-01-03 11:54:03 +0100

    Nouvelle organisation et utilisation du php
    Signed-off-by: sa <s.angeard@laposte.net>

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include 'header.html'; ?>
+ 
+ <div id="monEntete">
+   <h2><img src="logo.jpg" alt="logo du site" /> Mon application web
+       <img src="logo.jpg" alt="logo du site" /></h2>
+ </div>
+ 
+ <div id="maDivMenu">
+  <ul id="monMenu">
+   <li><a href="index.html" title="Accueil" class="current">Accueil</a></li>
+   <li><a href="index.html" title="Présentation">Présentation</a></li>
+   <li><a href="index.html" title="Articles">Articles</a></li>
+   <li><a href="index.html" title="Tutos">Tutos</a></li>
+   <li><a href="index.html" title="Forums">Forums</a></li>
+   <li><a href="index.html" title="Nous contacter">Nous contacter</a></li>
+  </ul>
+ </div> 
+ 
+<?php include 'calculatrice.html'; ?>
+<?php include 'footer.html'; ?>      
